aad8ceaea3b013ec6c0872204e0a0f6a602beb86
Suyash21101 <Suyash21101>
2022-11-02 16:14:54 +0000

    CI/CD
    Code was developed in databricks. Now i am committing code

---
+++ CI_CD_t1/Lib.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+def bubbleSort(arr):
+	n = len(arr)
+
+	# Traverse through all array elements
+	for i in range(n):
+
+		# Last i elements are already in place
+		for j in range(0, n-i-1):
+
+			# traverse the array from 0 to n-i-1
+			# Swap if the element found is greater
+			# than the next element
+			if arr[j] > arr[j+1]:
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+# COMMAND ----------
+
+def selectionSort(array, size):
+   
+    for step in range(size):
+        min_idx = step
+
+        for i in range(step + 1, size):
+         
+            # to sort in descending order, change > to < in this line
+            # select the minimum element in each loop
+            if array[i] < array[min_idx]:
+                min_idx = i
+         
+        # put min at the correct position
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
+
+
