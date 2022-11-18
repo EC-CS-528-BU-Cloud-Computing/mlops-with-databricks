da1e5421145f68b6f42a0c867b4311abfb909dfb
balajisath <74872762+balajisath@users.noreply.github.com>
2022-11-18 03:12:50 -0500

    Update test_sorts.py

--- CI_CD_t1/test_sorts.py
+++ CI_CD_t1/test_sorts.py
@@ -30,3 +30,17 @@ def selectionSort(array, size):
         # put min at the correct position
         (array[step], array[min_idx]) = (array[min_idx], array[step])
 
+	
+
+def insertionSort(arr):
+
+    n = len(arr)
+    for i in range(1, n):
+
+        key = arr[i]
+
+        j = i-1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
