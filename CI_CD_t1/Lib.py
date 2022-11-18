49ee7a047687326be5351516af5aea438252c390
balajisath <74872762+balajisath@users.noreply.github.com>
2022-11-18 03:11:46 -0500

    Update Lib.py

--- CI_CD_t1/Lib.py
+++ CI_CD_t1/Lib.py
@@ -33,3 +33,17 @@ def selectionSort(array, size):
         (array[step], array[min_idx]) = (array[min_idx], array[step])
 
 
+# COMMAND ----------
+
+def insertionSort(arr):
+
+    n = len(arr)
+    for i in range(1, n):
+
+        key = arr[i]
+
+        j = i-1
+        while j >=0 and key < arr[j] :
+                arr[j+1] = arr[j]
+                j -= 1
+        arr[j+1] = key
